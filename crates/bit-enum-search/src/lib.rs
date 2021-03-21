814fcf3e27bd44f84dee22a7643cbe70582394fb
Yuta Sadamori <yutapypy0930@gmail.com>
2021-03-21 18:03:25 +0900

    add dightex

---
+++ crates/bit-enum-search/src/lib.rs
@@ -0,0 +1,16 @@
+use bitset_fixed::BitSet;
+
+pub struct BitEnum {
+    cur: usize,
+    n: usize,
+}
+
+impl BitEnum {
+    fn new(n: usize) -> Self {
+        Self { cur: 0, n }
+    }
+
+    fn gen(&mut self) -> BitSet {
+        BitSet::
+    }
+}

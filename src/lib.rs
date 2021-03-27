fb1794b9cb51df78f06649ae56777fc9645f0a83
Yuta Sadamori <yutapypy0930@gmail.com>
2021-03-27 18:03:50 +0900

    apply digitex feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub use bitset::BitSet;
 pub use brute_bits::BruteBitsBuilder;
-pub use digitex::{Binary, Decimal, Digit, DigitBase, Hexdecimal, Octal};
+pub use digitex::{Binary, Decimal, StaticDigit as Digit, DigitBase, Hexdecimal, Octal};
 pub use prime::{erathosthnes::Erathosthnes, Prime as _, Sieve as _};
 pub use rle::RunLengthEncoding as _;
 pub use unique_count::UniqueCount as _;

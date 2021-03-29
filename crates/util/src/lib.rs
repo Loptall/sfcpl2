6eefbd77ca5945951b8a7b9fc031c2d04b855508
Yuta Sadamori <yutapypy0930@gmail.com>
2021-03-29 10:56:52 +0900

    add shuffle for util

--- crates/util/src/lib.rs
+++ crates/util/src/lib.rs
@@ -1,5 +1,7 @@
 //! This crate includes utility functions used by other workspace crates
 
+use rand::{thread_rng, Rng};
+
 /// convert (from..to) into (from, to)
 pub fn expand_range<R: std::ops::RangeBounds<usize>>(range: R, max: usize) -> (usize, usize) {
     let from = match range.start_bound() {
@@ -18,4 +20,29 @@ pub fn expand_range<R: std::ops::RangeBounds<usize>>(range: R, max: usize) -> (u
 /// return minimum `p` which applies `n <= 2 ^ p`
 pub fn ceil_pow(n: usize) -> usize {
     n.next_power_of_two().trailing_zeros() as usize
+}
+
+fn shuffle<T>(v: &mut [T]) {
+    let mut rng = thread_rng();
+    for i in (1..v.len()).rev() {
+        v.swap(rng.gen_range(0..=i), i);
+    }
+}
+
+pub trait Shuffle<T> {
+    fn shuffle(self) -> Self;
+}
+
+impl<T> Shuffle<T> for Vec<T> {
+    fn shuffle(mut self) -> Self {
+        shuffle(&mut self);
+        self
+    }
+}
+
+impl<T> Shuffle<T> for &mut [T] {
+    fn shuffle(self) -> Self {
+        shuffle(self);
+        self
+    }
 }
